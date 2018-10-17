8bbc3b9f413431257f8858c056aa103cde70a6a2
shubhamkar <shubhamkarayare@gmail.com>
2018-10-17 12:50:30 +0530

    Added Science and ICT in new site

--- create-site.el
+++ create-site.el
@@ -13,76 +13,84 @@
       (read-sequence contents stream)
       contents)))
 
-(defvar main-files (directory "data/main/Social Science/*.html"))
-(defvar footer-files (directory "data/footer/Social Science/*.html"))
+(defun create-site (subject-list)
+  (if (null subject-list)
+      'done
+      (let* ((main-files (directory
+                          (str:join "" (list "data/main/"
+                                             (car subject-list)
+                                             "/*.html"))))
+             (footer-files (directory
+                            (str:join "" (list "data/footer/"
+                                               (car subject-list)
+                                               "/*.html"))))
+             (out-folder (str:join "" (list (car subject-list) "/")))
+             (page-questions (file-get-contents
+                              (str:join "" (list "data/header/"
+                                                 (car subject-list)
+                                                 ".html"))))
+             (pq-dom (lquery:$ (initialize page-questions)))
+             (pq (lquery:$ ".page-question" (serialize)))
+             (*auto-escape* nil)
+             (len (length page-questions)))
+        (loop for i from 0 to len
+              for main-file in main-files
+              for footer-file in footer-files do
+              (let* ((page-question (aref pq i))
+                     (main (file-get-contents main-file))
+                     (footer (file-get-contents footer-file))
+                     (outfile (str:join "" (list out-folder
+                                                 (file-namestring main-file))))
+                     
+                     (to-write
+                       (html5
+                        (:head
+                         (:meta :charset "utf-8"
+                                :name "viewport"
+                                :content "width=device-width")
+                         (:link :rel "stylesheet"
+                                :href "../bootstrap-3.3.7-dist/css/bootstrap.min.css")
+                         (:link :rel "stylesheet"
+                                :href "../styles-bs.css")
+                         (:title (file-namestring main-file)))
+                        (:body
+                         (:nav :class "page-details"
+                               (:div :class "container"
+                                     (:div :class "row"
+                                           (:div :class "col-md-4 col-xs-3"
+                                                 :id "ahe_level"
+                                                 (:b :class "hidden-xs" "Level: ")
+                                                 "Level 0")
+                                           (:div :class "col-md-4 col-xs-6"
+                                                 :id "ahe_topic"
+                                                 (:b :class "hidden-xs" "Topic: ")
+                                                 "Social Science")
+                                           (:div :class "col-md-4 col-xs-3"
+                                                 :id "ahe_route"
+                                                 (:b  :class "hidden-xs" "Route Number: ")
+                                                 (format nil "~2,'0d" (+ i 1))))))
+                         page-question
+                         (:div :class "container"
+                               (:div :class "row"
+                                     (:div :class "col-md-8 col-sm-7" main (:br) (:br))
+                                     (:div :class "col-md-4 col-sm-5" footer)))
+                         (:div :class "visible-xs container-fluid" :id "bottom-bar"
+                               (:div :class "row"
+                                     (:div :id "follow-question-btn" :class "col-xs-9"
+                                           (:img :src "../Meta/question-circle-solid-red.png"
+                                                 :id "fa-question-circle")
+                                           "Would you like to know?")
+                                     (:div :id "map-button" :class "col-xs-3"
+                                           (:a :href "../Meta/List for Organized Study.html"
+                                               (:img :src "../Meta/map-solid-red.png"
+                                                     :id "fa-map-solid")))))
+                         (:script :src "../bootstrap-3.3.7-dist/jquery-3.2.1/jquery.min.js" "")
+                         (:script :src "../bootstrap-3.3.7-dist/js/bootstrap.min.js" "")
+                         (:script :src "../js-bs.js" "")))))
+                (with-open-file (out outfile :direction :output
+                                             :if-does-not-exist :create
+                                             :if-exists :overwrite)
+                  (format out to-write))))
+        (create-site (cdr subject-list)))))
 
-;; (defvar main (file-get-contents (car main-files)))
-;; (defvar footer (file-get-contents (car footer-files)))
-(defvar out-folder "Social Science/")
-(defvar page-questions (file-get-contents "data/header.html"))
-(defvar pq-dom (lquery:$ (initialize page-questions)))
-(defvar pq (lquery:$ ".page-question" (serialize)))
-;(defvar main (lquery:$ (initialize main-contents) (serialize)))
-;(defvar footer (lquery:$ (initialize footer-contents) (serialize)))
-
-(setq *auto-escape* nil)     ; this permits the tags in main and
-                                        ; footer to be evaluated
-(defvar len (length page-questions))
-(loop for i from 0 to len
-      for main-file in main-files
-      for footer-file in footer-files do
-      (let* ((page-question (aref pq i))
-             (main (file-get-contents main-file))
-             (footer (file-get-contents footer-file))
-             (outfile (str:join "" (list out-folder
-                                          (file-namestring main-file))))
-             
-             (to-write
-               (html5
-                (:head
-                 (:meta :charset "utf-8"
-                        :name "viewport"
-                        :content "width=device-width")
-                 (:link :rel "stylesheet"
-                        :href "../bootstrap-3.3.7-dist/css/bootstrap.min.css")
-                 (:link :rel "stylesheet"
-                        :href "../styles-bs.css")
-                 (:title (file-namestring main-file)))
-                (:body
-                 (:nav :class "page-details"
-                       (:div :class "container"
-                             (:div :class "row"
-                                   (:div :class "col-md-4 col-xs-3"
-                                         :id "ahe_level"
-                                         (:b :class "hidden-xs" "Level: ")
-                                         "Level 0")
-                                   (:div :class "col-md-4 col-xs-6"
-                                         :id "ahe_topic"
-                                         (:b :class "hidden-xs" "Topic: ")
-                                         "Social Science")
-                                   (:div :class "col-md-4 col-xs-3"
-                                         :id "ahe_route"
-                                         (:b  :class "hidden-xs" "Route Number: ")
-                                         (format nil "~2,'0d" (+ i 1))))))
-                 page-question
-                 (:div :class "container"
-                       (:div :class "row"
-                             (:div :class "col-md-8 col-sm-7" main (:br) (:br))
-                             (:div :class "col-md-4 col-sm-5" footer)))
-                 (:div :class "visible-xs container-fluid" :id "bottom-bar"
-                       (:div :class "row"
-                             (:div :id "follow-question-btn" :class "col-xs-9"
-                                   (:img :src "../Meta/question-circle-solid-red.png"
-                                         :id "fa-question-circle")
-                                   "Would you like to know?")
-                             (:div :id "map-button" :class "col-xs-3"
-                                   (:a :href "../Meta/List for Organized Study.html"
-                                       (:img :src "../Meta/map-solid-red.png"
-                                             :id "fa-map-solid")))))
-                 (:script :src "../bootstrap-3.3.7-dist/jquery-3.2.1/jquery.min.js" "")
-                 (:script :src "../bootstrap-3.3.7-dist/js/bootstrap.min.js" "")
-                 (:script :src "../js-bs.js" "")))))
-        (with-open-file (out outfile :direction :output
-                                     :if-does-not-exist :create
-                                     :if-exists :overwrite)
-          (format out to-write))))
+(create-site '("Social Science" "Information and Communication Technology" "Science"))
--- data/pull.el
+++ data/pull.el
@@ -10,42 +10,50 @@
       (read-sequence contents stream)
       contents)))
 
-(defvar contents)
-(defvar outfile)
-(defvar infiles (directory "../../AHAE.Handset/Social Science/*.html"))
-(defvar outfolder-main "main/Social Science/")
-(defvar outfolder-footer "footer/Social Science/")
+(defun pull-for-subjects (subject-list)
+  (if (null subject-list)
+      'done
+      (let* ((infolder (str:join ""
+                                 (list "../" (car subject-list))))
+             (header-file (str:join ""
+                                    (list "header/" (car subject-list) ".html")))
+             (infiles (directory (str:join ""
+                                           (list infolder "/" "*.html"))))
+             (outfolder-main (str:join ""
+                                       (list "main/" (car subject-list) "/")))
+             (outfolder-footer (str:join ""
+                                         (list "footer/" (car subject-list) "/"))))
+        (with-open-file (header header-file :direction :output
+                                            :if-exists :overwrite
+                                            :if-does-not-exist :create)
+          (loop for infile in infiles do
+                (let* ((outfile-main (str:join ""
+                                               (list outfolder-main
+                                                     (file-namestring infile))))
+                       (outfile-footer (str:join ""
+                                                 (list outfolder-footer
+                                                       (file-namestring infile))))
+                       (contents (file-get-contents infile))
+                       (dom (lquery:$ (initialize contents)))
+                       (main-to-write (lquery:$ dom "main" (serialize)))
+                       (footer-to-write (lquery:$ dom "footer" (serialize)))
+                       (header-to-write (lquery:$ dom ".page-question" (serialize))))
+                  (with-open-file (main outfile-main :direction :output
+                                                     :if-exists :overwrite
+                                                     :if-does-not-exist :create)
+                    (if (> (length main-to-write) 0)
+                        (format main (aref main-to-write 0))
+                        NIL))
+                  (with-open-file (footer outfile-footer :direction :output
+                                                         :if-exists :overwrite
+                                                         :if-does-not-exist :create)
+                    (if (> (length footer-to-write) 0)
+                        (format footer (aref footer-to-write 0))
+                        NIL))
+                  (if (> (length header-to-write) 0)
+                      (format header (aref header-to-write 0))))))
+        (pull-for-subjects (cdr subject-list)))))
 
-(defvar header-file "header.html")
-
-(with-open-file (header header-file :direction :output
-                             :if-exists :overwrite
-                             :if-does-not-exist :create)
-  (loop for infile in infiles do
-        (let* ((outfile-main (str:join ""
-                                       (list outfolder-main
-                                             (file-namestring infile))))
-               (outfile-footer (str:join ""
-                                         (list outfolder-footer
-                                               (file-namestring infile))))
-               (contents (file-get-contents infile))
-               (dom (lquery:$ (initialize contents)))
-               (main-to-write (lquery:$ dom "main" (serialize)))
-               (footer-to-write (lquery:$ dom "footer" (serialize)))
-               (header-to-write (lquery:$ dom ".page-question" (serialize))))
-          (with-open-file (main outfile-main :direction :output
-                                             :if-exists :overwrite
-                                             :if-does-not-exist :create)
-            (if (> (length main-to-write) 0)
-                (format main (aref main-to-write 0))
-                NIL))
-          (with-open-file (footer outfile-footer :direction :output
-                                                 :if-exists :overwrite
-                                                 :if-does-not-exist :create)
-            (if (> (length footer-to-write) 0)
-                (format footer (aref footer-to-write 0))
-                NIL))
-          (if (> (length header-to-write) 0)
-              (format header (aref header-to-write 0))))))
+(pull-for-subjects '("Social Science" "Information and Communication Technology" "Science"))
 
 
